4bcc591e46e116069d7e9ae32e5b22ea0a9f371e
Shehroz <you@example.com>
2025-05-25 20:31:08 +0500

    patch: relationship

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -8,8 +8,13 @@ class CartItem extends Model
 {
     protected $fillable = ['cart_id', 'product_id', 'quantity'];
 
+//    public function product()
+//    {
+//        return $this->belongsTo(Product::class);
+//    }
+    // app/Models/CartItem.php
     public function product()
     {
-        return $this->belongsTo(Product::class);
+        return $this->belongsTo(Product::class, 'product_id');
     }
 }
